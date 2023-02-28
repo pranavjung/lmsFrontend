276d6630d82730f16c35d39f1911a86221d52903
PranavJ <pranavjungade@outlook.com>
2023-02-28 17:14:53 +0530

    Rearranged Files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Component/Home';
-import Login from './Component/Login';
+import React from 'react'
+// import Home from './Component/Home';
+// import Login from './Component/Login';
+import Navbar from './Component/Navbar';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return(
-    <BrowserRouter>
-      <Routes>
-          <Route index element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-    </BrowserRouter>
+  // return(
+  //   <BrowserRouter>
+  //     <Routes>
+  //         <Route index element={<Login />} />
+  //         <Route path="/home" element={<Home />} />
+  //       </Routes>
+  //   </BrowserRouter>
+  // );
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
   );
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
 }
 
 export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,9 @@
+import Logo from "../../public/Logo-Infoway-Cdac.jpeg";
+
+export default function Navbar() {
+    return (
+        <div>
+            <img src=""/>
+        </div>
+    )
+}
