d9970ef3499cea432f35f31fe50592463df1a712
PranavJ <pranavjungade@outlook.com>
2023-03-02 16:44:34 +0530

    Complete Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,9 +1,27 @@
-import Logo from "../../public/Logo-Infoway-Cdac.jpeg";
+import Logo from "../Images/InfowayLogo.jpeg";
+import {FaBars, FaTimes} from "react-icons/fa";
 
 export default function Navbar() {
     return (
-        <div>
-            <img src=""/>
-        </div>
+        <header>
+            <img src={Logo} alt="infoway logo"/>
+            <nav>
+                <a href="/#">Home</a>
+                <a href="/#">Attendence</a>
+                <a href="/#">Courseware</a>
+                <a href="/#">Exam</a>
+                <a href="/#">Result</a>
+                <a href="/#">Performance Analysis</a>
+                <a href="/#">Profile</a>
+                <a href="/#">Assignments</a>
+                <a href="/#">Logout</a>
+                <button>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button>
+                <FaBars />
+            </button>
+        </header>
     )
 }
